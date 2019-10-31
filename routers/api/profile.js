667c761f553f75e7a2a96a404b73eb32ae6431b5
Balbalnom <diandianhanxing@gmail.com>
2019-10-30 18:47:06 -0700

    profile backend done with github api update functionality

--- routers/api/profile.js
+++ routers/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const{ check, validationResult} = require('express-validator');
@@ -340,8 +342,36 @@ router.delete('/education/:edu_id', auth, async(req, res) =>{
         console.error(err.message);
         res.status(500).send('Server Error');
     }
-})
+});
 
+
+//@route PUT api/profile/github/:username
+//@desc get user repos from Github
+//@access Public 
+
+
+router.get('/github/:username', (req, res) =>{
+    try{
+        const options ={
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js'}
+        }
+
+        request(options, (error, response, body) =>{
+            if(error) console.error(error);
+            
+            if(response.statusCode !== 200){
+                return res.status(404).json({ msg: 'No Github Profile found'});
+            }
+
+            res.json(JSON.parse(body));
+        });
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 
 
 
