4221afc00ac0088fea1b920671ce7f7ca22e44a7
Balbalnom <diandianhanxing@gmail.com>
2019-11-05 21:22:32 -0800

    finishing login/reg in React

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
     return (
@@ -7,12 +8,12 @@ const Landing = () => {
         <div className="landing-inner">
           <h1 className="x-large">Developer Connector</h1>
           <p className="lead">
-            Create a developer profile/portfolio, share posts and get help from
+            Create Link developer profile/portfolio, share posts and get help from
             other developers
           </p>
           <div className="buttons">
-            <a href="register.html" className="btn btn-primary">Sign Up</a>
-            <a href="login.html" className="btn btn-light">Login</a>
+            <Link to="/register" className="btn btn-primary">Sign Up</Link>
+            <Link to="/login" className="btn btn-light">Login</Link>
           </div>
         </div>
       </div>
